385641301259a6f500febe6371b08fbd5b129302
sielavic <63396872+sielavic@users.noreply.github.com>
2022-05-23 23:23:13 +0300

    Update hide.js

--- hide.js
+++ hide.js
@@ -15,3 +15,24 @@ $(".js-attachment-list").each(function() {
                     .prevAll('div')
                     .show();
       });
+
+
+
+
+
+$(document).on('click','#icon-pencil_' + card_id,  function () {
+            var targetId = $(this).attr('id');
+            var cardId = targetId.replace(/[^0-9]/g, "");
+            $('#show_quick_card_' + cardId).hide();
+            $('.portlet-body').css('background', '#0009');
+            $('#card_reaload_' + <?= $card->macrotasknote->parent_id ?> +' '+ '#window_' + cardId).removeClass("hide").addClass("show-modal-card");
+            $('#card_reaload_' + <?= $card->macrotasknote->parent_id ?> +' '+ '#window_button_' + cardId).removeClass("hide").addClass("show-modal-card");
+      });
+      $(document).on('click','#close_card_' + card_id, function () {
+            $('.portlet-body').css('background', '');
+            var targetId = $(this).attr('id');
+            var cardId = targetId.replace(/[^0-9]/g,"");
+            $('#card_reaload_' + <?= $card->macrotasknote->parent_id ?> +' '+ '#window_' + cardId).removeClass("show-modal-card").addClass("hide");
+            $('#card_reaload_' + <?= $card->macrotasknote->parent_id ?> +' '+ '#window_button_' + cardId).removeClass("show-modal-card").addClass("hide");
+            $('#show_quick_card_' + cardId).show();
+      });
